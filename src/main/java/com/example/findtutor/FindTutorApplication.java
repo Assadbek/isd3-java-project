013c6353a2fe7e6e63d48c9b363dd6dfd5240054
Kapayev Assabek <assadbekkapayev@gmail.com>
2022-11-25 00:35:29 +0600

    simple kafka added

--- src/main/java/com/example/findtutor/FindTutorApplication.java
+++ src/main/java/com/example/findtutor/FindTutorApplication.java
@@ -1,10 +1,22 @@
 package com.example.findtutor;
 
+import com.example.findtutor.model.Tutor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@EnableKafka
 @SpringBootApplication
 public class FindTutorApplication {
+
+    @KafkaListener(topics="newTutor")
+    public void orderListener(ConsumerRecord<Long, Tutor> record){
+        System.out.println(record.partition());
+        System.out.println(record.key());
+        System.out.println(record.value());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FindTutorApplication.class, args);
--- src/main/java/com/example/findtutor/controller/TutorController.java
+++ src/main/java/com/example/findtutor/controller/TutorController.java
@@ -3,7 +3,9 @@ package com.example.findtutor.controller;
 import com.example.findtutor.model.Tutor;
 import com.example.findtutor.service.impl.TutorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 @RequestMapping("tutors")
 public class TutorController {
+
+    @Autowired
+    private KafkaTemplate<Long, Tutor> kafkaTemplate;
 
     private final TutorServiceImpl tutorsService;
 
@@ -29,41 +34,14 @@ public class TutorController {
         return tutorsService.loadTutor(id);
     }
 
-    @GetMapping("/create-tutor")
-    public String create(Model model) {
-        return "create-tutor";
+    @PostMapping
+    public void createTutor(@RequestBody Tutor tutor) {
+        // save tutor
+        System.out.println(tutor);
+
+        ListenableFuture<SendResult<Long, Tutor>> future = kafkaTemplate.send("newTutor", 1L, tutor);
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 
-    @GetMapping("/{id}/edit")
-    public String editTutor(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("tutor", tutorsService.loadTutor(id));
-        return "edit-tutor";
-    }
-
-    @PostMapping("/{id}")
-    public String update(@ModelAttribute("person") Tutor tutor,
-                         @PathVariable("id") Long id) {
-        tutorsService.saveTutor(tutor);
-        return "redirect:/tutors";
-    }
-
-    @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") Long id) {
-        tutorsService.deleteTutor(id);
-        return "redirect:/tutors";
-    }
-
-    /*@PostMapping()
-    public String createTutor(@RequestParam String name,
-                              @RequestParam String surname,
-                              @RequestParam Long subject,
-                              @RequestParam int age) {
-        Tutor.TutorBuilder newTutor = Tutor.builder();
-        newTutor.name(name)
-                .surname(surname)
-                .age(age);
-
-        tutorsService.saveTutor(newTutor.build());
-        return "redirect:/tutors";
-    }*/
 }
